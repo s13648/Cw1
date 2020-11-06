d81fd27006b011e14d812339716c24ebe27b9333
Misha Kurch <m.kurch@yahoo.com>
2020-11-06 22:42:46 +0100

    Pierwszy commit

---
+++ Cw1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    static class Program
+    {
+        private const string EmailPattern = @"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*";
+
+        static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(args[0]);
+            var content = await response.Content.ReadAsByteArrayAsync();
+            var data = Encoding.UTF8.GetString(content);
+            var extractEmails = ExtractEmails(data);
+
+        }
+
+        private static IEnumerable<string> ExtractEmails(string data)
+        {
+            var emailRegex = new Regex(EmailPattern, RegexOptions.IgnoreCase);
+
+            return emailRegex
+                .Matches(data)
+                .Select(n => n.Value)
+                .ToList();
+        }
+    }
+}
